e35a7af02107a059e02576d2a0491988ded42f63
Dominika Szukala-Kosinska <dominika.szukala@gmail.com>
2024-04-02 21:59:40 +0200

    Add imput

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [films, setFilms] = useState("");
+
   return (
-    <div>Hello there!</div>
+    <div>
+      <h1>Film search</h1>
+      <label id="film_search">
+        <input type="text" name="film_search" value={films} onChange={e => setFilms(e.target.value)}/>
+      </label>
+    </div>
   )
 }
 

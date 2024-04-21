913d06692a68a2e9d594411cfb5751c4a3a9d94d
Dominika Szukala-Kosinska <dominika.szukala@gmail.com>
2024-04-21 19:21:16 +0200

    Remove axios and install swr

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import { Input } from './components';
-import axios from 'axios';
 
 type Film = {
   title: string;
@@ -11,28 +10,17 @@ function App() {
   const [inputValue, setInputValue] = useState("");
   const [films, setFilms] = useState<Film[]>([])
 
-  const fetchFilms = async () => {
-    try {
-      const response = await axios(`http://www.omdbapi.com/?t=${inputValue}`);
-      setFilms(response.data);
-    } catch (err) {
-      console.error(err);
-    }
-  };
 
-  useEffect(() => {
-    fetchFilms()
-  }, [inputValue])
 
   return (
     <div>
       <h1>Film search</h1>
       <Input value={inputValue} onInputChange={(e) => setInputValue(e.target.value)}/>
-      {films.length > 0 ? films.map(film => (
+      {/* {films.length > 0 ? films.map(film => (
         <h2>
           {film.title}
         </h2>
-      )) : null}
+      )) : null} */}
     </div>
   )
 }

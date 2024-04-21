2435952ffabf2ee1a78d2078472cf96966283a6c
Dominika Szukala-Kosinska <dominika.szukala@gmail.com>
2024-04-21 19:45:12 +0200

    Use SWR

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import './App.css'
 import { Input } from './components';
+import { fetcher } from './utils';
+import useSWR from 'swr';
 
 type Film = {
   title: string;
@@ -8,12 +10,14 @@ type Film = {
 
 function App() {
   const [inputValue, setInputValue] = useState("");
-  const [films, setFilms] = useState<Film[]>([])
+  const [films, setFilms] = useState<Film[]>([]);
+  const { data, isLoading, error } = useSWR(`http://www.omdbapi.com/?s=${inputValue}&apikey=${import.meta.env.VITE_API_KEY}`, fetcher);
 
 
+  if (error) return "An error occurred";
 
   return (
-    <div>
+    <div> 
       <h1>Film search</h1>
       <Input value={inputValue} onInputChange={(e) => setInputValue(e.target.value)}/>
       {films.length > 0 ? films.map(film => (
@@ -25,4 +29,4 @@ function App() {
   )
 }
 
-export default App
+export default App;

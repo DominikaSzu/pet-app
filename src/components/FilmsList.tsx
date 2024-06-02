affdf577b9333778ae2195c1b8ac1ca34fc31423
Dominika Szukala-Kosinska <dominika.szukala@gmail.com>
2024-06-02 13:23:36 +0200

    Cleanup

--- src/components/FilmsList.tsx
+++ src/components/FilmsList.tsx
@@ -1,18 +1,16 @@
 import { Film, FilmType } from "./Film";
-import { NoResults } from "./NoResults";
 
 export type Props = {
-  films: FilmType[];
+    films: FilmType[];
 };
 
 export const FilmsList = ({ films }: Props) => {
-  return (
-    <div>
-      {films && films.length === 0 ? (
-        <NoResults />
-      ) : (
-        films.map((film) => <Film key={film.imdbID} {...film} />)
-      )}
-    </div>
-  );
+    const noFilmsFound = !films || films.length === 0;
+    return (
+        <div>
+            {noFilmsFound
+                ? null
+                : films.map((film) => <Film key={film.imdbID} {...film} />)}
+        </div>
+    );
 };

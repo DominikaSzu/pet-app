1d9bc1ccc8982b47542a8d007eb07eef57491c15
Dominika Szukala-Kosinska <dominika.szukala@gmail.com>
2024-06-02 13:12:44 +0200

    Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 import './App.css'
-import { Input, FilmType, FilmsList } from './components';
+import { Input, FilmsList } from './components';
 import { fetcher } from './utils';
 import useSWR from 'swr';
 
 function App() {
   const [inputValue, setInputValue] = useState("");
-  const [films, setFilms] = useState<FilmType[]>([]);
   const { data, isLoading, error } = useSWR(`http://www.omdbapi.com/?s=${inputValue}&apikey=${import.meta.env.VITE_API_KEY}`, fetcher);
-
-  useEffect(() => {
-    setFilms(data?.Search)
-  }, [inputValue])
 
   const onChangeHandle = (e: ChangeEvent<HTMLInputElement>) => setInputValue(e.target.value);
 
@@ -20,7 +15,7 @@ function App() {
     <div> 
       <h1>Film search</h1>
       <Input value={inputValue} onInputChange={onChangeHandle}/>
-      {isLoading ? <div>"Loading..."</div> : <FilmsList films={films}/>}
+      {isLoading ? <div>"Loading..."</div> : <FilmsList films={data.Search}/>}
     </div>
   )
 }
--- src/components/Film.tsx
+++ src/components/Film.tsx
@@ -1,10 +1,16 @@
 export type FilmType = {
     Title: string;
+    Year: string;
+    Poster: string;
     imdbID: string;
 }
 
 export const Film = (film: FilmType) => (
+<div>
+    <img src={film.Poster} alt={`Poster of movie: ${film.Title}`}/>
 <h2>
     {film.Title}
 </h2>
+<p>{film.Year}</p>
+</div>
 )
